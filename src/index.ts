21029cdffd27cad89c142f8cf236373ce978a730
NoodleRobot <NoodleRobotDev>
2024-07-17 22:43:35 -0500

    feat: added search flags for model and type

--- src/index.ts
+++ src/index.ts
@@ -8,57 +8,102 @@ import figlet from "figlet";
 
 const program = new Command();
 
-console.log(figlet.textSync("Dir Manager"));
+console.log(figlet.textSync("Device CLI Tool"));
 
 program
   .version("1.0.0")
-  .description("An example CLI for managing a directory")
-  .option("-l, --ls  [value]", "List directory contents")
-  .option("-m, --mkdir <value>", "Create a directory")
-  .option("-t, --touch <value>", "Create a file")
-  .option("-d, --device-list <value>", "List all devices")
+  .description("CLI tool for listing and searching devices")
+  .option("-l, --list-all-devices", "List all devices")
+  .option("-f, --list-family <value>", "List devices matching a family name")
+  .option("-m, --list-model <value>", "List devices matching a model name")
+  .option("-t, --list-type <value>", "List devices matching a type")
   .parse(process.argv);
 
 const options = program.opts();
 
-async function listDirContents(filepath: string) {
-  try {
-    const files = await fs.promises.readdir(filepath);
-    const detailedFilesPromises = files.map(async (file: string) => {
-      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
-      const { size, birthtime } = fileDetails;
-      return { filename: file, "size(KB)": size, created_at: birthtime };
-    });
-    const detailedFiles = await Promise.all(detailedFilesPromises);
-    console.table(detailedFiles);
-  } catch (error) {
-    console.error("Error occurred while reading the directory!", error);
-  }
+function listAllDevices() {
+  fs.readFile("./data/device-data.json", "utf8", (err, data) => {
+    try {
+      const deviceJsonData = JSON.parse(data);
+      console.table(deviceJsonData);
+    } catch (error: any) {
+      console.error(`Error reading file: ${error.message}`);
+    }
+  });
 }
 
-function createDir(filepath: string) {
-  if (!fs.existsSync(filepath)) {
-    fs.mkdirSync(filepath);
-    console.log("The directory has been created successfully");
-  }
+function listDevicesByFamily(familyName: string) {
+  fs.readFile("./data/device-data.json", "utf8", (err, data) => {
+    try {
+      const filteredDevicesFamily = JSON.parse(data).filter(
+        (d: any) => d.family === familyName,
+      );
+      if (filteredDevicesFamily.length > 0) {
+        console.table(filteredDevicesFamily);
+      } else {
+        emptyResult();
+      }
+    } catch (error: any) {
+      console.error(`Error reading file: ${error.message}`);
+    }
+  });
 }
 
-function createFile(filepath: string) {
-  fs.openSync(filepath, "w");
-  console.log("An empty file has been created");
+function listDevicesByModel(modelName: string) {
+  fs.readFile("./data/device-data.json", "utf8", (err, data) => {
+    try {
+      const filteredDevicesByModel = JSON.parse(data).filter(
+        (d: any) => d.model === modelName,
+      );
+      if (filteredDevicesByModel.length > 0) {
+        console.table(filteredDevicesByModel);
+      } else {
+        emptyResult();
+      }
+    } catch (error: any) {
+      console.error(`Error reading file: ${error.message}`);
+    }
+  });
 }
 
-if (options.ls) {
-  const filepath = typeof options.ls === "string" ? options.ls : __dirname;
-  listDirContents(filepath);
+function listDevicesByType(typeName: string) {
+  fs.readFile("./data/device-data.json", "utf8", (err, data) => {
+    try {
+      const filteredDevicesByType = JSON.parse(data).filter(
+        (d: any) => d.type === typeName,
+      );
+      if (filteredDevicesByType.length > 0) {
+        console.table(filteredDevicesByType);
+      } else {
+        emptyResult();
+      }
+    } catch (error: any) {
+      console.error(`Error reading file: ${error.message}`);
+    }
+  });
 }
 
-if (options.mkdir) {
-  createDir(path.resolve(__dirname, options.mkdir));
+function emptyResult() {
+  console.warn("No devices found");
 }
 
-if (options.touch) {
-  createFile(path.resolve(__dirname, options.touch));
+if (options.listAllDevices) {
+  listAllDevices();
+}
+
+if (options.listFamily) {
+  const familyName: string = options.listFamily;
+  listDevicesByFamily(familyName);
+}
+
+if (options.listModel) {
+  const modelName: string = options.listModel;
+  listDevicesByModel(modelName);
+}
+
+if (options.listType) {
+  const typeName: string = options.listType;
+  listDevicesByType(typeName);
 }
 
 if (!process.argv.slice(2).length) {
